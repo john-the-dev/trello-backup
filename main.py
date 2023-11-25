f1b895d4207157cded1e2edddb8b0177b29aa9ad
john-the-dev <52230987+john-the-dev@users.noreply.github.com>
2023-11-24 23:35:45 -0800

    example how to use json2txt

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import trellobackup
-import config as config
+import configme as config
 
 def main():
+    # trello = trellobackup.TrelloBackup(config.apiKey, config.appToken)
+    # trello.enableBackupAttachments(config.backupAttachments)
+    # trello.run()
+
     trello = trellobackup.TrelloBackup(config.apiKey, config.appToken)
-    trello.enableBackupAttachments(config.backupAttachments)
-    trello.run()
+    trello.json2txt("/users/ruiwang/Downloads/trello_work_board_20231124.json")
 
 if __name__ == '__main__':
     # Execute only if run as a script.

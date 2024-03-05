7fa800ec511e7f58c294b72d9370d62d966e1161
john-the-dev <52230987+john-the-dev@users.noreply.github.com>
2024-03-05 13:44:13 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import trellobackup
 import configme as config
 
 def main():
-    # trello = trellobackup.TrelloBackup(config.apiKey, config.appToken)
-    # trello.enableBackupAttachments(config.backupAttachments)
-    # trello.run()
-
     trello = trellobackup.TrelloBackup(config.apiKey, config.appToken)
-    trello.json2txt("/users/ruiwang/Downloads/trello_work_board_20231124.json")
+    trello.enableBackupAttachments(config.backupAttachments)
+    trello.run()
+
+    # trello = trellobackup.TrelloBackup(config.apiKey, config.appToken)
+    # trello.json2txt("/users/ruiwang/Downloads/trello_work_board_20231124.json")
 
 if __name__ == '__main__':
     # Execute only if run as a script.
